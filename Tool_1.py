fc9fd3a233e705417446d33a4d8b58c8daebd8cf
Winson <39163110+isanmu@users.noreply.github.com>
2018-06-26 01:21:27 +0800

    Initial commit

---
+++ Tool_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# Author: Winson
+# User: sanmu
+# Date: 2018/6/26 0:02
+# Project: MyTools
+# File Name: Tool_1.py
+
+import markdown
+
+markdown.markdown()
+
+
---
+++ demands.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# Author: Winson
+# User: sanmu
+# Date: 2018/6/25 21:04
+# Project: MyTools
+# File Name: demands.py
+
+
+class DemandClass:
+    def __init__(self):
+        pass
+
+    def targets(self):
+        pass
