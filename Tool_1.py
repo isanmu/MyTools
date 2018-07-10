7626a5fa1d1cc312a2c25e9ff1da487ae48abcc5
Winson <39163110+isanmu@users.noreply.github.com>
2018-07-11 00:02:26 +0800

    # add ui,main_tools

--- Tool_1.py
+++ Tool_1.py
@@ -11,25 +11,107 @@ import pyperclip
 # import win32clipboard
 
 
-def star_unordered_list():
-    # TODO: add optional input to get unordered symbol
+def star_unordered_list(symbol='·', plus=1, direct=1):
     text = pyperclip.paste()
-    lines = text.split('\n')
-    # for line in lines:
-    #     lines[lines.index(line)] = '\t😁 ' + line
+    print(repr(text))
+    if '\r\n' in text:
+        lines = text.split('\r\n')
+    elif '\n' in text:
+        lines = text.split('\n')
+    else:
+        lines = text
+    # print(lines)
     # '\t◉ '; '\t• '; '\t😁 '...... (by 'shift+ctrl+B' under ch)
-
-    # for i in range(len(lines)):
-    #     lines[i] = '* ' + lines[i]
-
+    if symbol == 'smile':
+        symbol = '😁'
+    elif symbol == '·':
+        symbol = '•'
     for i, line in enumerate(lines):
-        lines[i] = '\t😁 ' + line
-    pyperclip.copy('\n'.join(lines))
+        if plus == 1:
+            if direct >= 1:
+                lines[i] = '\t' + symbol + ' ' + line
+            elif direct == 0:
+                # line = line.rstrip()
+                lines[i] = line + symbol
+        elif plus == 0:
+            if symbol in line:
+                if direct == 2:
+                    lines[i] = line.replace(str(symbol), '')
+                elif direct == 1:
+                    lines[i] = line[:len(symbol)].replace(str(symbol), '')\
+                               + line[len(symbol):]
+                elif direct == 0:
+                    lines[i] = line[-len(symbol):].replace(str(symbol), '')\
+                               + line[:-len(symbol)]
+    pyperclip.copy('\r\n'.join(lines))
     return lines
 
 
-start = time.time()
-# print(pyperclip.is_available())
-print(star_unordered_list())
-end = time.time()
-print('run time:', end-start)
+def self_var_list():
+    #
+    text = pyperclip.paste()
+    if '\r\n' in text:
+        lines = text.split('\r\n')
+    elif '\n' in text:
+        lines = text.split('\n')
+    else:
+        lines = text
+    # lines = text.split()
+    print(lines)
+    newlines = []
+    for i, line in enumerate(lines):
+        if '//' in line:
+            newlines.append('# ' + line.replace('//', ''))
+        else:
+            varline = line.split()
+            # print(varline)
+        # line = line.lstrip()
+            for j, var in enumerate(varline):
+                if ',' in var:
+                    var = var.replace(',', '')
+                elif ';' in var:
+                    var = var.replace(';', '')
+                newlines.append('self.' + var + ' = ' + var)
+    pyperclip.copy('\n'.join(newlines))
+    print(newlines)
+    return newlines
+
+
+def init_var_list(num):
+    if num <= 0:
+        raise ValueError('Wrong input num(to list)')
+    else:
+        #
+        text = pyperclip.paste()
+        print(repr(text))
+        if '\r\n' in text:
+            lines = text.split('\r\n')
+        elif '\n' in text:
+            lines = text.split('\n')
+        else:
+            lines = text
+        print(lines)
+        # lines = text.split()
+        newlines = []
+        for i, line in enumerate(lines):
+            if '//' not in line:
+                varline = line.split()
+                for var in varline:
+                    # line = line.lstrip()
+                    newlines.append(var)
+        print(newlines[:10])
+        for i, var in enumerate(newlines, 1):
+            if i % (num+1) == 0:
+                newlines.insert(i-1, '\n')
+        pyperclip.copy(' '.join(newlines))
+        return newlines
+
+
+if __name__ == '__main__':
+    text_new = star_unordered_list(symbol='1', plus=0, direct=1)
+    print(text_new)
+    start = time.time()
+    # print(pyperclip.is_available())
+    # print(star_unordered_list())
+    end = time.time()
+    print('run time:', end-start)
---
+++ main_tools.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 7/4/2018 10:19
+# @Author  : Winson
+# @User    : winswang
+# @Site    : 
+# @File    : main_tools.py
+# @Project : MyTools
+# @Software: PyCharm
+
+import sys
+
+import pyperclip
+from PyQt5 import QtCore, QtGui, QtWidgets
+# from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtWidgets import QMessageBox, QTableWidgetItem, qApp
+
+from ui_files.mytools_gui import Ui_MainWindow
+from Tool_1 import init_var_list, self_var_list, star_unordered_list
+
+
+class MyMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def to_list(self):
+        try:
+            num = self.var_numer_of_line.value()
+            init_var_list(num)
+            print('Done: to_list(%s)!' % num)
+        except Exception as e:
+            show_mainwindow_error(self, e)
+
+    def to_self_equal(self):
+        try:
+            self_var_list()
+            print('Done: to_self_equal !')
+        except Exception as e:
+            show_mainwindow_error(self, e)
+
+    def text_symbol(self):
+        try:
+            symbol = self.symbolBox.currentText()
+            # print(repr(symbol))
+            plus = 1 if self.plusBox.currentText() == '+' else 0
+            print('plus= ', plus)
+            direct = self.directBox.currentText()
+            # assert str(direct) is 'all'
+            if direct == 'left':
+                direct = 1
+            elif direct == 'all':
+                direct = 2
+            elif direct == 'right':
+                direct = 0
+            print('direct=', direct)
+            new_text = star_unordered_list(str(symbol), plus=plus, direct=direct)
+            print('Done: add_nonorder_symbol !')
+            print('Result:', new_text)
+        except Exception as e:
+            show_mainwindow_error(self, e)
+
+    def add_copy(self):
+        text = self.copytext.toPlainText()
+        pyperclip.copy(text)
+
+
+def show_mainwindow_error(win, error):
+    QMessageBox.information(win, "Error", str(error) + '\n', QMessageBox.Ok)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    mywindow = MyMainWindow()
+    mywindow.show()
+    sys.exit(app.exec_())
